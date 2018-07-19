4e195ec8657ebf063e66b23ea09efc02a3576d8b
bananalytics <31481575+bananalytics@users.noreply.github.com>
2018-07-19 17:39:51 +0200

    Completed Ch2 - templates

--- app/routes.py
+++ app/routes.py
@@ -5,4 +5,22 @@ from app import app
 @app.route('/index')
 def index():
 	user = {'username':'Filip'}
-	return render_template('index.html', title="Home", user=user)
+	posts = [
+		{
+			'author' : {'username': 'Jan'},
+			'body' : 'I love the OBX!'
+		},
+		{
+			'author' : {'username': 'Magda'},
+			'body' : 'Look at this amazing sunrise. I SOOO needed this beach holiday!'
+		},
+		{
+			'author' : {'username': 'Michal'},
+			'body' : 'Surf\'s up dude!'
+		},
+		{
+			'author': {'username': 'Bartus'},
+			'body': 'Getting mad inspo for my sneaker line'
+		}
+	]
+	return render_template('index.html', title="Home", user=user, posts=posts)
